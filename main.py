0ff8eb5ec4aa6321e5e737bc3589624a43bc6eb2
gamererr <55727914+gamererr@users.noreply.github.com>
2020-11-23 21:51:01 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import random
+import asyncio
+import discord
+import requests
+from discord import Member, Embed
+
+with open("tokenfile", "r") as tokenfile:
+	token=tokenfile.read()
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+async def attachments_to_files(attached,spoiler=False):
+	filelist = []
+	for i in attached:
+		file = await i.to_file()
+		filelist.insert(len(filelist),file)
+	return filelist
+
+@client.event
+async def on_ready():
+	print("hello world!")
+
+@client.event
+async def on_message(message):
+    return
+
+client.run(token)
